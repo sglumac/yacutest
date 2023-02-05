6fdd8facae2df44362d4100175c4525de5018371
Slaven Glumac <slaven.glumac@gmail.com>
2023-02-05 16:27:12 +0100

    further refactor for more stable tests4tests

--- tests4tests/failures.c
+++ tests4tests/failures.c
@@ -11,17 +11,10 @@ static void file_report_on_test_finished(YacuReportState state, YacuStatus resul
 {
     FileReport *fileReport = state;
 
-    switch (result)
-    {
-    case TEST_FAILURE:
-    {
-        FILE *reportFile = fopen(fileReport->filePath, "w");
-        fputs(message, reportFile);
-        fflush(reportFile);
-        fclose(reportFile);
-        break;
-    }
-    }
+    FILE *reportFile = fopen(fileReport->filePath, "w");
+    fputs(message, reportFile);
+    fflush(reportFile);
+    fclose(reportFile);
 }
 
 static void file_report_on_suite_finished(YacuReportState state) {}
@@ -30,13 +23,7 @@ static void file_report_on_suites_finished(YacuReportState state) {}
 
 typedef void ForkedAction(YacuTestRun *forkedTestRun);
 
-static void main_action(YacuTestRun *testRun, YacuStatus forkReturnCode, const char *forkMessage, const char* expectedMessage)
-{
-    YACU_ASSERT_EQ_INT(testRun, forkReturnCode, TEST_FAILURE);
-    YACU_ASSERT_IN_STR(testRun, expectedMessage, forkMessage);
-}
-
-void test_using_fork(YacuTestRun *testRun, const char *reportPath, const char* expectedMessage, ForkedAction forkedAction)
+void test_using_fork(YacuTestRun *testRun, const char *reportPath, ForkedAction forkedAction, char* failureMessage)
 {
     FileReport fileReportState = {.filePath = reportPath};
     YacuReport fileReport = {
@@ -52,14 +39,13 @@ void test_using_fork(YacuTestRun *testRun, const char *reportPath, const char* e
         forkedAction(&forkedTestRun);
     }
     YacuStatus forkReturnCode = wait_for_forked(pid);
-    char forkMessage[YACU_TEST_RUN_MESSAGE_MAX_SIZE];
     FILE *reportFile = fopen(reportPath, "r");
-    fgets(forkMessage, YACU_TEST_RUN_MESSAGE_MAX_SIZE, reportFile);
+    fgets(failureMessage, YACU_TEST_RUN_MESSAGE_MAX_SIZE, reportFile);
     fclose(reportFile);
-    main_action(testRun, forkReturnCode, forkMessage, expectedMessage);
+    YACU_ASSERT_EQ_INT(testRun, forkReturnCode, TEST_FAILURE);
 }
 
-void forked_assert_failed_cmp_int(YacuTestRun* forkedTestRun)
+void forked_assert_failed_cmp_int(YacuTestRun *forkedTestRun)
 {
     int small = -1;
 
@@ -69,7 +55,10 @@ void forked_assert_failed_cmp_int(YacuTestRun* forkedTestRun)
 void test_assert_failed_cmp_int(YacuTestRun *testRun)
 {
     const char reportPath[] = "failedCmpIntTest.log";
-    test_using_fork(testRun, reportPath, "failures.c:70 - Assertion small < -2 (-1 < -2) failed!", forked_assert_failed_cmp_int);
+    char failureMessage[YACU_TEST_RUN_MESSAGE_MAX_SIZE];
+    test_using_fork(testRun, reportPath, forked_assert_failed_cmp_int, failureMessage);
+    YACU_ASSERT_IN_STR(testRun, "failures.c:", failureMessage);
+    YACU_ASSERT_IN_STR(testRun, " - Assertion small < -2 (-1 < -2) failed!", failureMessage);
 }
 
 YacuTest assertionFailuresTests[] = {
